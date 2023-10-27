88998e4beed9a397a7817ffd433d8fe91b056f8d
Yujie Wang <52476612+yyoumaa@users.noreply.github.com>
2023-10-27 15:24:03 +0800

    Create heap_buffer_overflow.c

---
+++ divide/heap_buffer_overflow.c
@@ -0,0 +1,9 @@
+//clang++ heap_buffer_overflow.c -o hbo
+//./hbo
+int main(int argc, char **argv) {
+  int *array = new int[100];
+  array[0] = 0;
+  int res = array[argc + 100];  // BOOM
+  delete [] array;
+  return res;
+}
