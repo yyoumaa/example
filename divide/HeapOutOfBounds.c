fab58a1fee0a5ec96ef5323f8f5b0083458aa118
Yujie Wang <52476612+yyoumaa@users.noreply.github.com>
2023-10-27 15:20:04 +0800

    Create HeapOutOfBounds.c

---
+++ divide/HeapOutOfBounds.c
@@ -0,0 +1,9 @@
+//clang HeapOutOfBounds.c -o hoob 
+// ./hoob
+int main(int argc, char **argv) {
+  int *array = new int[100];
+  array[0] = 0;
+  int res = array[argc + 100];  // BOOM
+  delete [] array;
+  return res;
+}
